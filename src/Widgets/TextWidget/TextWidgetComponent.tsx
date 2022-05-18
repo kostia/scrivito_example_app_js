467d3d16939d3a8099fc415b6388feff53491a2e
David Császár <dcsaszar@users.noreply.github.com>
2022-05-18 12:35:30 +0200

    TS TextWidget

--- src/Widgets/TextWidget/TextWidgetComponent.tsx
+++ src/Widgets/TextWidget/TextWidgetComponent.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import * as Scrivito from "scrivito";
 import { alignmentClassName } from "../../utils/alignmentClassName";
+import TextWidget from "./TextWidgetClass";
 
-Scrivito.provideComponent("TextWidget", ({ widget }) => (
+Scrivito.provideComponent(TextWidget, ({ widget }) => (
   <Scrivito.ContentTag
     tag="div"
     className={alignmentClassName(widget.get("alignment"))}
